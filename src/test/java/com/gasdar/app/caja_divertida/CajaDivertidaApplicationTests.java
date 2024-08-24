cd371413cc276aded5ffd89ae3d23a94704889a6
emartinez <eliu.martinez@connect360.cl>
2024-08-24 19:49:18 -0400

    inicializando estructura del proyecto

---
+++ src/test/java/com/gasdar/app/caja_divertida/CajaDivertidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gasdar.app.caja_divertida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CajaDivertidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
